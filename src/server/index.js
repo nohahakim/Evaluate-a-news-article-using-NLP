1d8cad071807f5d159279c889f7af390a8979349
nohahakim <raafatabass@hotmail.com>
2025-01-07 10:14:01 +0300

    update

--- src/server/index.js
+++ src/server/index.js
@@ -1,62 +1,58 @@
-var path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require("cors")
-const mockAPIResponse = require('./mockAPI.js')
+var path = require("path");
+const express = require("express");
+const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mockAPIResponse = require("./mockAPI.js");
 const axios = require("axios");
 
-
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 //new
-const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key='
-const apiKey = process.env.API_KEY
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1?key=";
+const apiKey = process.env.API_KEY;
 console.log(`Your API Key is ${apiKey}`);
 
+const app = express();
 
+app.use(cors());
+app.use(express.static("dist"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+console.log(__dirname);
 
-
-const app = express()
-
-app.use(cors())
-app.use(express.static('dist'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-console.log(__dirname)
-
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+  console.log("Example app listening on port 8081!");
+});
 
-
-
-
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
 
 // Post Method
-app.post('/api', async (req, res) => {
-    userInput = req.body.url;
-    const mcURL = `${baseURL}${apiKey}&url=${userInput}&lang=en`
+app.post("/api", async (req, res) => {
+  userInput = req.body.url;
+  const mcURL = `${baseURL}${apiKey}&url=${userInput}&lang=en`;
 
-    axios.post(mcURL).then((response) => {
-        const mcResults = response.data
+  axios.post(mcURL).then((response) => {
+    const mcResults = response.data;
 
-
-        try {
-            res.send(mcResults);
-        }
-        catch (error) {
-            console.log(`an error occured: ${error}`)
-        }
-    })
-})
-
+    try {
+      res.send(mcResults);
+    } catch (error) {
+      console.log(`an error occured: ${error}`);
+    }
+  });
+});
+app.get("/api/news", (req, res) => {
+  // Your existing logic from mockAPI.js or index.js
+  res.json({ message: "News data" });
+});
+module.exports = app;
+module.exports.handler = serverless(app);
